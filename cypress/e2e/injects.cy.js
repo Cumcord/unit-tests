7871cf5d6880fe93bd8f8d1407c765be55c85111
yellowsink <yellowsink@riseup.net>
2022-06-07 19:12:21 +0100

    add a test woa

--- cypress/e2e/injects.cy.js
+++ cypress/e2e/injects.cy.js
@@ -1,5 +1,11 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://discord.com/app')
+describe('cumcord', () => {
+  it('injects', () => {
+    cy.visit('https://discord.com/app');
+    cy.get(`[name="email"]`).type(Cypress.env("username"))
+    cy.get(`[name="password"]`).type(Cypress.env("password"))
+    cy.get(`[type="submit"]`).click();
+
+    // it can take max 1 minute to log in before crash
+    cy.get(`[class*="panels-]"`, { timeout: 60_000 });
   })
 })
